37f584d8ec09fda90658fff4a7b6c4785bb30209
Hostile-Entity <jackal2898@gmail.com>
2020-10-22 22:17:35 -0400

    Complete change

---
+++ KV2-SURVIVAL/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * File name: CameraScript.cs
+ * This file is part of KV-2 Survival game
+ * 
+ * Created by Iurii Guliev
+ * Last modified: Mar 4, 2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ KV2-SURVIVAL/Assets/Scripts/Forest.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * File name: Forest.cs
+ * This file is part of KV-2 Survival game
+ * 
+ * Created by Iurii Guliev
+ * Last modified: Mar 10, 2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Game.ForestList.Add(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
